1b796a9f1e372dd9c41c4fd44ee33116b7cf96e1
yogupta <yogeshgupta@capgemini.com>
2016-07-25 17:06:53 +0530

    Added createDate

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
@@ -1,13 +1,15 @@
 package io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api;
 
-import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
-import io.oasp.gastronomy.restaurant.general.dataaccess.api.ApplicationPersistenceEntity;
-import io.oasp.gastronomy.restaurant.offermanagement.common.api.Offer;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
+import io.oasp.gastronomy.restaurant.general.dataaccess.api.ApplicationPersistenceEntity;
+import io.oasp.gastronomy.restaurant.offermanagement.common.api.Offer;
 
 /**
  * The {@link ApplicationPersistenceEntity persistent entity} for a special.
@@ -21,6 +23,8 @@ public class SpecialEntity {
   private String name;
 
   private OfferEntity offer;
+
+  private Date createdDate;
 
   @Embedded
   private WeeklyPeriodEmbeddable activePeriod;
@@ -106,6 +110,22 @@ public class SpecialEntity {
   public void setSpecialPrice(Money specialPrice) {
 
     this.specialPrice = specialPrice;
+  }
+
+  /**
+   * @return createdDate
+   */
+  public Date getCreatedDate() {
+
+    return this.createdDate;
+  }
+
+  /**
+   * @param createdDate new value of {@link #getcreatedDate}.
+   */
+  public void setCreatedDate(Date createdDate) {
+
+    this.createdDate = createdDate;
   }
 
 }
