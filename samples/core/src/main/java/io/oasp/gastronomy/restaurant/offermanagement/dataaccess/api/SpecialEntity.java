63642776bf3178317dccebed3e7a78e2a8854813
Manoj <manoj.ghag@capgemini.com>
2016-07-26 14:25:12 +0530

    Exc 3

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
@@ -5,7 +5,10 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
 import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
@@ -64,6 +67,7 @@ public class SpecialEntity extends ApplicationPersistenceEntity implements Speci
    *
    * @return offer {@link Offer} this special applies for.
    */
+  @ManyToOne
   public OfferEntity getOffer() {
 
     return this.offer;
@@ -126,6 +130,7 @@ public class SpecialEntity extends ApplicationPersistenceEntity implements Speci
    * @return created
    */
   @Override
+  @Temporal(TemporalType.TIMESTAMP)
   public Date getCreated() {
 
     return this.created;
